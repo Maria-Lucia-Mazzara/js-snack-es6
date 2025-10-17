f7f2b7136f148fb18be659a1039f35b1dd0d66e0
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-17 15:10:59 +0200

    creata array di bici

--- Snack1/script.js
+++ Snack1/script.js
@@ -1,1 +1,14 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
 console.dir(document);
+
+const biciclette = [
+  { nome: "rossa", peso: 7.5 },
+  { nome: "blu", peso: 6.9 },
+  { nome: "verde", peso: 8.1 },
+  { nome: "arancione", peso: 6.8 },
+  { nome: "nera", peso: 7.2 }
+];
+
