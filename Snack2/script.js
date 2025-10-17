48aa3b20e37871ea9595885ec021584b3a79f1eb
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-17 16:09:28 +0200

    array snack 2 fatta

--- Snack2/script.js
+++ Snack2/script.js
@@ -5,3 +5,14 @@
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 console.dir(document);
+
+
+// Array di squadre con nome, punti e falli (punti e falli inizialmente 0)
+const squadre = [
+    { nome: 'Juventus', punti: 0, falli: 0 },
+    { nome: 'Inter', punti: 0, falli: 0 },
+    { nome: 'Milan', punti: 0, falli: 0 },
+    { nome: 'Napoli', punti: 0, falli: 0 },
+    { nome: 'Roma', punti: 0, falli: 0 }
+];
+
